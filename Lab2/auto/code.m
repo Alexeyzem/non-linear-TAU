41dc108122005c25a27aeaaf54ab92919a946f57
Alexey <alex.zem2015@mail.ru>
2024-04-14 21:08:59 +0300

    Lab2 and 3 autp

--- Lab2/auto/code.m
+++ Lab2/auto/code.m
@@ -2,20 +2,20 @@ clc;
 clear all;
 close all;
                   
-XMAX = 15;                    
+XMAX = 20;                    
 STEP = 0.5;               
-TMAX = 100;                  
-X0 = 0.1;
-Y0 = -0.1;
-Z0 = 0.1;
+TMAX = 95;                  
+X0 = -1;
+Y0 = -0;
+Z0 = 0.5;
 
 dx = @(t, x) funcGrad(t, x);
 event_out_of_bounds = @(t, z) outOfBounds(t, z, XMAX+0.01);
 plotLocus(X0, Y0, Z0, dx, event_out_of_bounds, TMAX); % строим фазовые траектории
 
 function dxdt = funcGrad(t, x)
-gama = 0.1;
-alpha = 0.05;
+gama = 0.87;
+alpha = 1.1;
 %
 %
 dxdt(1, :) = x(2,:).*(x(3,:) - 1 + x(1, :).^2) + gama.*x(1, :);
