17f0cfe00c740aabe60f7965ffd7258c053849a9
Alexey <alex.zem2015@mail.ru>
2024-05-05 18:51:24 +0300

    init commit

---
+++ Lab6/auto/scripts/DeInit.m
@@ -0,0 +1,36 @@
+clear Project;
+clear out;
+
+clearTrash();
+
+function clearTrash()
+    all_models = ls('./models/*.slx');
+    warning ('off','all');
+
+    for i = 1:size(all_models, 1)
+       model_name = strip(all_models(i,:));
+       model_name = model_name(1:length(model_name)-4);
+
+       delete(strcat(model_name, '.elf'));
+       delete(strcat(model_name, '.slxc'));
+       delete(strcat(model_name, '.zip'));
+
+       if isfolder('./slprj')
+           rmdir('./slprj', 's');
+       end
+    end
+    
+    all_models = ls('./');
+    for i = 1:size(all_models, 1)
+       model_name = strip(all_models(i,:));
+       model_name = model_name(1:length(model_name)-4);
+
+       delete(strcat(model_name, '.elf'));
+       delete(strcat(model_name, '.slxc'));
+       delete(strcat(model_name, '.zip'));
+
+       if isfolder('./slprj')
+           rmdir('./slprj', 's');
+       end
+    end
+end
---
+++ Lab6/auto/scripts/init.m
@@ -0,0 +1,6 @@
+Project.widthPulse = 0.7;
+Project.widthNonPulse = 0.3;
+Project.inertia = 4000;
+Project.maxDeg = 360;
+Project.minDeg = -360;
+Project.peredatka = 50;
